03a4533da5b13601e226ebec33b20e0593bff488
Faareha59 <your-email@example.com>
2025-02-14 16:16:02 +0500

    Add initial implementation of Cars array and print_data function

---
+++ example week 2.js
@@ -0,0 +1,12 @@
+let Cars = [
+    { name: "Tesla", model: 145 },
+    { name: "Honda", model: 678 },
+    { name: "Suzuki", model: 35 }
+];
+
+function print_data(item) {
+    console.log(item);
+}
+
+let new_arr = Cars.map(print_data);
+console.log(new_arr);
